efb863fdbdfdf9063bec115face05b4b3866624d
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-09-19 20:16:12 +0300

    Add one of the solutions to the problems.

---
+++ solution.py
@@ -0,0 +1,14 @@
+def less_than_50():
+    number_50 = 50
+    my_number = None
+    if number_50 > 100:
+        # if number_50 is greater than 100, assign the `my_number` variable to the number 100
+        my_number = 100
+    elif number_50 > 50:
+        # if number_50 is greater than 50, assign the `my_number` variable to the number 50
+        my_number = 50
+    else:
+        # else assign the `my_number` variable to 0
+        my_number = 0
+
+
